badc68361f9d2cb055e5e00ea039d65b969c51dd
ranagithubrr <mr6430119@gmail.com>
2023-11-23 14:21:55 +0600

    Feat: Captain add api complete

--- routes/captains.js
+++ routes/captains.js
@@ -6,17 +6,34 @@ const Captains = require('../models/captainsModel');
 router.get('/', (req, res) => {
     res.send("I am from captains route")
 });
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { name, roll, semester, phone, email } = req.body;
-    if(name == undefined || roll == undefined || semester == undefined || phone == undefined || email == undefined){
-        res.status(400).json({msg:"Please provide all data"});        
+    if (name === undefined || roll === undefined || semester === undefined || phone === undefined || email === undefined) {
+        return res.status(400).json({ msg: "Please provide all data" });
     }
-    console.log(roll);
     const capobj = {
         name, roll, semester, phone, email
     }
-    res.status(200).json({msg:"got data", capobj})
+    const existingCaptain = await Captains.findOne({ roll });
+    if (existingCaptain) {
+        return res.status(400).json({ msg: "Captain with this roll already exists" });
+    }
+    try {
+        const Data = await Captains.create({
+            ...capobj
+        });
+
+        if (Data) {
+            return res.status(200).json({ msg: "Captain Added", Data });
+        } else {
+            return res.status(400).json({ msg: "Failed to Add Captain" });
+        }
+    } catch (err) {
+        console.error('Error creating captain:', err);
+        return res.status(500).json({ msg: "Server error" });
+    }
 });
+
 
 
 
