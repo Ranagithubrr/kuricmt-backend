3e802fecc240a2c88b5a30f49dff70dc3c4234d0
ranagithubrr <mr6430119@gmail.com>
2023-11-23 14:40:14 +0600

    Feat: Captain delete api created

--- routes/captains.js
+++ routes/captains.js
@@ -2,10 +2,16 @@ const express = require('express')
 const router = express.Router();
 const Captains = require('../models/captainsModel');
 
-
-router.get('/', (req, res) => {
-    res.send("I am from captains route")
+// get all captains
+router.get('/', async (req, res) => {
+    const AllCaptains = await Captains.find({});
+    if (AllCaptains) {
+        res.status(200).json({ msg: "Got Data", AllCaptains })
+    } else {
+        res.status(400).json({ msg: "Failed to retrive data" })
+    }
 });
+// add captain
 router.post('/', async (req, res) => {
     const { name, roll, semester, phone, email } = req.body;
     if (name === undefined || roll === undefined || semester === undefined || phone === undefined || email === undefined) {
@@ -33,6 +39,64 @@ router.post('/', async (req, res) => {
         return res.status(500).json({ msg: "Server error" });
     }
 });
+
+// delete captain
+router.post('/delete/:id', async (req, res) => {
+    const captainId = req.params.id;
+    try {
+        const deletedCaptain = await Captains.findOneAndDelete({ _id: captainId });
+        if (deletedCaptain) {
+            res.status(200).json({ msg: "Captain deleted", deletedCaptain });
+        } else {
+            res.status(404).json({ msg: "Captain not found" });
+        }
+    } catch (error) {
+        console.error('Error deleting captain:', error);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+// update captain
+router.post('/update/:id', async (req, res) => {
+    const captainId = req.params.id;
+    const { name, roll, semester, phone, email } = req.body;
+
+    // Check if any required data is missing
+    if (name === undefined || roll === undefined || semester === undefined || phone === undefined || email === undefined) {
+        return res.status(400).json({ msg: "Please provide all data" });
+    }
+    const existingCaptain = await Captains.findById(captainId);
+
+    if (!existingCaptain) {
+        return res.status(404).json({ msg: "Captain not found" });
+    }
+
+    const updatedCaptainData = {
+        name: name || existingCaptain.name,
+        roll: roll || existingCaptain.roll,
+        semester: semester || existingCaptain.semester,
+        phone: phone || existingCaptain.phone,
+        email: email || existingCaptain.email
+    };
+
+    try {
+        const updatedCaptain = await Captains.findOneAndUpdate(
+            { _id: captainId },
+            { $set: updatedCaptainData },
+            { new: true }
+        );
+
+        if (updatedCaptain) {
+            res.status(200).json({ msg: "Captain updated", updatedCaptain });
+        } else {
+            res.status(404).json({ msg: "Captain not found" });
+        }
+    } catch (error) {
+        console.error('Error updating captain:', error);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
 
 
 
