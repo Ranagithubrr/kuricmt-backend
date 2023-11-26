c216407295a288efed309c7a79dc4a16fa3dc6c5
ranagithubrr <mr6430119@gmail.com>
2023-11-26 12:53:54 +0600

    Fix: notice route protected

--- routes/notice.js
+++ routes/notice.js
@@ -4,10 +4,11 @@ const { verifyToken } = require('../middleware/jwtmiddleware');
 const Notices = require('../models/noticeModel');
 
 // Post a notice
-router.post('/', async (req, res) => {
-    const { title, description, noticeurl } = req.body;  
-    if(!title || !description || !noticeurl){
-      return res.status(400).json({msg:"Fill all the fields"})
+router.post('/', verifyToken, async (req, res) => {
+  if (req.user && req.user.user && req.user.user.type === 'admin') {
+    const { title, description, noticeurl } = req.body;
+    if (!title || !description || !noticeurl) {
+      return res.status(400).json({ msg: "Fill all the fields" })
     }
     const NoticeObj = {
       title,
@@ -17,20 +18,24 @@ router.post('/', async (req, res) => {
     const Data = await Notices.create({
       ...NoticeObj
     });
-    if(Data){
-      return res.status(200).json({msg:"Notice Uploaded", Data})
-    }else{
-      return res.status(400).json({msg:"Failed to Upload"})
+    if (Data) {
+      return res.status(200).json({ msg: "Notice Uploaded", Data })
+    } else {
+      return res.status(400).json({ msg: "Failed to Upload" })
     }
+  }
+  else {
+    return res.status(400).json({ msg: "access denied" })
+  }
 });
 
 // get all notices 
-router.get('/', async (req,res)=>{
-    const AllNotices = await Notices.find({});
-    if(AllNotices){
-       return res.status(200).json({msg:"Got Notices", AllNotices})
-    }
-    res.status(400).json({msg:"Cant get"})
+router.get('/', async (req, res) => {
+  const AllNotices = await Notices.find({});
+  if (AllNotices) {
+    return res.status(200).json({ msg: "Got Notices", AllNotices })
+  }
+  res.status(400).json({ msg: "Cant get" })
 });
 
 // get the single notice
